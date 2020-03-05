fb15c1d98f414fd660c4fc7dd89b6ec40be1b5e7
Adrien Pillou <44286944+adrienpillou@users.noreply.github.com>
2020-03-05 21:01:06 +0100

    Scrollbar and styling

--- interface.py
+++ interface.py
@@ -1,70 +1,78 @@
 from tkinter import *
-import serverwrapper
+from serverwrapper import Server
 
-root = Tk()
-root.title("PyCraft")
-root.iconbitmap("./assets/terminal_bloc.ico")
-root.resizable(width=False,height=False)
-root.geometry("300x400+760+390")
-frame = Frame(root)
-refreshRate = 1000 #(ms)
-
+# Updating the player list
 def UpdatePlayerListbox():
     playerListBox.delete(0,END)
     for item in server.GetPlayerList():
         playerListBox.insert(END, item)
 
+# Updating app interface
 def UpdateInterface():
-    if server.isOnline :
+    available = server.TestIfServerIsAvailable()
+    if available:
         UpdatePlayerListbox()
         slotsLabel['text'] = "Slots : {0}".format(server.GetSlotsInfos())
         pingLabel['text'] = "Ping : {0}ms".format(str(server.GetLatency()))
-    elif not server.isOnline:
+            
+    elif not available:
         playerListBox.delete(0,END)
         slotsLabel['text'] = "Slots : -/-"
         pingLabel['text'] = "Ping : -ms"
-
-    server.TestIfServerIsAvailable()
+    
     root.after(refreshRate, UpdateInterface)
-
-def EstablishConnection(ip, port):
-    print("Establishing connection to the server on {0}:{1}...".format(ip,port))
-    myServer = serverwrapper.Server(ip,port)
-    myServer.TestIfServerIsAvailable()
-    if(myServer.isOnline):
+    
+def EstablishConnection(server):
+    print("Establishing connection to the server on {0}:{1}...".format(server.ip,server.port))
+    server = Server(server.ip,server.port)
+    server.TestIfServerIsAvailable()
+    if(server.isOnline):
         print("This server is online.")
     else:
         print("This server is currently offline.")
-    return myServer
+    UpdateInterface()
+
+root = Tk()
+root.title("PyCraft")
+root.iconbitmap("./terminal_bloc.ico")
+root.resizable(width=False,height=False)
+root.geometry("300x400+760+390")
+frame = Frame(root)
+frame.pack()
+refreshRate = 1000 #(ms)
+
+
+server = Server("127.0.0.1", "25565") # Default connexion settings
 
 # Creating widgets elements
-ipAddressLabel = Label(frame, text="Server address : ")
-portLabel = Label(frame,text="Server port : ")
+ipAddressLabel = Label(frame, text="Server address : ", font="Helvetica 10 bold")
+portLabel = Label(frame,text="Server port : ", font="Helvetica 10 bold")
 ipAddressField = Entry(frame)
 ipAddressField.insert(0, "127.0.0.1")
 portAddressField = Entry(frame)
 portAddressField.insert(0,"25565")
-slotsLabel = Label(frame , text ="Slots : -/-")
-pingLabel = Label(frame, text = "Ping : -ms")
-submitButton = Button(frame, text="Query", command = lambda:EstablishConnection(ipAddressField.get(), portAddressField.get()))
-
-playerListLabel = Label(frame, text="Connected Players :")
+slotsLabel = Label(frame , text ="Slots : -/-", font="Helvetica 10 bold")
+pingLabel = Label(frame, text = "Ping : -ms", font="Helvetica 10 bold")
+submitButton = Button(frame, text="Query", command = lambda:EstablishConnection(server))
+playerListLabel = Label(frame, text="Connected Players :", font="Helvetica 10 bold")
 playerListBox = Listbox(frame)
 
 # Positionning elements on a grid
-ipAddressLabel.grid(row = 0, column = 0)
-ipAddressField.grid(row = 1, column = 0)
-portLabel.grid(row = 2, column = 0)
-portAddressField.grid(row = 3, column = 0)
-submitButton.grid(row = 4,column = 0)
-slotsLabel.grid(row = 5, column = 0)
-pingLabel.grid(row = 6, column = 0)
-playerListLabel.grid(row=7, column=0)
-playerListBox.grid(row =8, column = 0)
+ipAddressLabel.pack()
+ipAddressField.pack()
+portLabel.pack()
+portAddressField.pack()
+submitButton.pack()
+slotsLabel.pack()
+pingLabel.pack()
+playerListLabel.pack()
+playerListBox.pack(side="left",fill="y")
 
-frame.place(relx = .5, rely = .5, anchor = CENTER)
+listBoxScrollBar = Scrollbar(frame, orient="vertical")
+listBoxScrollBar.config(command=playerListBox.yview)
+listBoxScrollBar.pack(side="right", fill="y")
+playerListBox.config(yscrollcommand=listBoxScrollBar.set)
 
-server = EstablishConnection(ipAddressField.get(), portAddressField.get())
-
+#server = serverwrapper.Server("93.25.248.66", "25565")
 UpdateInterface()
 root.mainloop()
